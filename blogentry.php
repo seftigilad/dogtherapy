311cbfba61b48d5eda7b7984ae3c5ced9154623b
seftigilad <gilsefti@gmail.com>
2026-05-01 21:58:32 +0300

    a

--- blogentry.php
+++ blogentry.php
@@ -192,6 +192,15 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
-    
+
+  <a
+    href="https://wa.me/972526416831?text=%D7%A9%D7%9C%D7%95%D7%9D%2C%20%D7%94%D7%92%D7%A2%D7%AA%D7%99%20%D7%93%D7%A8%D7%9A%20%D7%94%D7%90%D7%AA%D7%A8%20%D7%95%D7%90%D7%A0%D7%99%20%D7%9E%D7%A2%D7%95%D7%A0%D7%99%D7%99%D7%9F%2F%D7%AA%20%D7%91%D7%A4%D7%A8%D7%98%D7%99%D7%9D%20%D7%A2%D7%9C%20%D7%9B%D7%9C%D7%91%D7%A0%D7%95%D7%AA%20%D7%98%D7%99%D7%A4%D7%95%D7%9C%D7%99%D7%AA"
+    class="float-whatsapp"
+    target="_blank"
+    rel="noopener"
+    aria-label="פנייה בוואטסאפ">
+    <span class="fa fa-whatsapp"></span>
+  </a>
+
   </body>
 </html>
